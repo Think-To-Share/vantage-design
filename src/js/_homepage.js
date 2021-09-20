aea9fc1bcf5f4e38d9afcb10619bfada7e977bb9
Rahul Dey <rahul900day@gmail.com>
2021-09-20 19:15:19 +0530

    added testimonial slider

---
+++ src/js/_homepage.js
@@ -0,0 +1,12 @@
+import { tns } from "tiny-slider/src/tiny-slider";
+
+if(document.querySelector('.testimonials-section')) {
+    tns({
+        container: '.testimonials-section .testimonials',
+        items: 1,
+        nav: false,
+        controlsContainer: '.testimonials-section .testimonial-nav',
+        autoplay: true,
+        autoplayButtonOutput: false
+    })
+}
--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,3 @@
 require('../scss/app.scss')
 
-let array = [20, 2, 12, 32]
-
-array.map((v) => v > 15)
-
-console.log('hii')
+require('./_homepage');
